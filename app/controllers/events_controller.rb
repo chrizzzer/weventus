12ffa8ee970ac0c993ace8420f7f5d5e7d438eb1
Corey <corfitzgerald@gmail.com>
2015-02-07 09:25:54 -0500

    Added more CRUD items

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,11 +10,31 @@ class EventsController < ApplicationController
   def new
   end
 
+  def edit
+    @event = Event.find(params[:id])
+  end
   def create
     @event = Event.new(event_params)
 
     @event.save
     redirect_to @event
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to events_path
   end
 
   private
